13e62beaec43ba81cb7b63d77cc881c8da17aad2
quanphan-orz <157004072+quanphan-orz@users.noreply.github.com>
2024-01-18 16:22:12 +0700

    Create Snippet.cpp

---
+++ Snippet.cpp
@@ -0,0 +1,73 @@
+/*
+	(づ￣ 3￣)づ 
+	Code by: Anh Quan Phan
+	From: BL1 Secondary School 22/12/2009  
+	    P   D   A   Q
+	--TLE RTE WA help me!--
+*/
+
+#pragma GCC optimize("Ofast")
+
+
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+
+//__________________________________________________________________
+//______________________________DEFINE______________________________
+#define __FILE(name) if (fopen(name".inp", "r")) freopen(name".inp", "r", stdin), freopen(name".out", "w", stdout);
+#define v vector
+#define db double
+#define ll long long
+#define ull unsigned long long
+#define big __int128
+#define fix(a, b) fixed <<setprecision(a) <<(b)
+#define FOR(i, x, n) for (int i = (x); i <= (n); ++i)
+#define F1(i, n) for (int i = 1; i <= (n); ++i)
+#define F0(i, n) for (int i = 0; i < (n); ++i)
+#define maxn(x) *max_element(x.begin(), x.end())
+#define minn(x) *min_element(x.begin(), x.end())
+#define TIME (1.0 * clock() / CLOCKS_PER_SEC)
+#define el "\n"
+#define gcd(a, b) abs(__gcd(a, b))
+#define lcm(a, b) abs((a) * (b) / gcd(a, b))
+#define printvec(x) copy(x.begin(), x.end(), ostream_iterator<int>(cout, " "));
+#define out(a) for (int i = 1; i <= (n); ++i) cout <<a[i] <<' '
+#define all1(x) (x) + (1), (x) + (n) + (1)
+#define all(x) x.begin(),x.end()
+#define pii pair<int, int> 
+#define pob pop_back()
+#define eb emplace_back
+#define pb push_back
+#define fi first
+#define se second
+#define __PDAQ__ int main()
+//__________________________________________________________________
+//____________________________SOLVE_________________________________
+/*
+		Suc vat thieu nang!
+	Inp: 
+	Out: 
+	Explain:
+	--> 
+	Now let's start coding! 
+*/
+//__________________________________________________________________
+//___________________________CONSTANT_______________________________
+const int MAXN = 1e7 + 1;
+const int MOD = 1e9 + 7;
+//__________________________________________________________________
+//___________________________FUNCTION_______________________________
+
+//__________________________________________________________________
+//____________________________BEGIN_________________________________
+__PDAQ__{
+	ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+	__FILE("PDAQ");
+	cerr <<"Time elapsed: " <<TIME <<"s.\n";
+	return (0^0);
+}
+//__________________________________________________________________
+//_____________________________END__________________________________
